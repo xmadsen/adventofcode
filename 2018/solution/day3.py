f00c80fd8d45ec04b7295a59ecae4880b7bc465f
xmadsen <xander.madsen@gmail.com>
2018-12-03 08:52:12 -0500

    not right but close for part1

--- 2018/solution/day3.py
+++ 2018/solution/day3.py
@@ -19,11 +19,31 @@ with open(input_file) as file:
 # Part 1
 
 
+def get_all_tuples(xloc, yloc, xsize, ysize):
+    all_tuples = []
+    for dx in range(xsize):
+        for dy in range(ysize):
+            all_tuples.append((xloc+dx, yloc+dy))
+    return all_tuples
+
+
 def part1():
+    global id_data
     start = time.time()
+    grid_occurrences = {}
+    for id in id_data:
+        these_tuples = get_all_tuples(
+            id['xloc'], id['yloc'], id['xsize'], id['ysize'])
+        for this_tuple in these_tuples:
+            if this_tuple not in grid_occurrences:
+                grid_occurrences[this_tuple] = 1
+            else:
+                grid_occurrences[this_tuple] += 1
+
+    two_pluses = sum([True for item in grid_occurrences if item >= 2])
 
     end = time.time()
-    return end-start
+    return two_pluses, end-start
 
 # Part 2
 
