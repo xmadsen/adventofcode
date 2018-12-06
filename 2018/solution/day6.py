f59292898af395cee9dce1d42da339ff16701063
xmadsen <xander.madsen@gmail.com>
2018-12-06 18:49:12 -0500

    first day6 commit

---
+++ 2018/solution/day6.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys
+import time
+from itertools import cycle
+
+input_file = sys.argv[1]
+
+with open(input_file) as file:
+    input_values = [line.strip('\n') for line in file.readlines()]
+
+# Part 1
+
+
+def get_smallest_encompassing_square(set_of_points):
+
+
+def part1():
+    start = time.time()
+
+    end = time.time()
+    return, end-start
+
+# Part 2
+
+
+def part2():
+    return
+
+
+p1answer, p1time = part1()
+p2answer, p2time = part2()
+print("    Part 1 : {}\n             {:.5f}s".format(p1answer, p1time))
+print("    Part 2 : {}\n             {:.5f}s".format(p2answer, p2time))
