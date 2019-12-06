0faa6a31dab3f3ea65e1afeb4443b0214506bc5b
xmadsen <xander.madsen@gmail.com>
2019-12-06 08:24:20 -0500

    Add day5 progress

--- 2019/solution/day5.py
+++ 2019/solution/day5.py
@@ -12,32 +12,45 @@ with open(input_file) as file:
 # Part 1
 
 
-def part1():
+def part1(input):
+    output = []
     # print(input_values)
     intcode = list(map(int, input_values))
-    i = 0
-    while i in range(len(intcode)):
-        instruction = intcode[i]
+    #intcode[0] = 1
+    pointer = 0
+    while pointer in range(len(intcode)):
+        instruction = intcode[pointer]
         opcode = int(str(instruction)[-2:])
+        print("Pointer: {}".format(pointer))
+        print("Instruction: {}".format(instruction))
         param_modes = list(
-            map(int, list(''.join(reversed(str(instruction)[-2:])))))
+            map(int, list(''.join(reversed(str(instruction)[:-1])))))
+        param_modes = list(zip(map(0, param_modes, range(3))))
         print("Param_modes: {}".format(param_modes))
-        i += 4
         if opcode == 99:
             return intcode[0]
         elif opcode == 1:
-            intcode[intcode[i+3]] = intcode[intcode[i+1]] +\
-                intcode[intcode[i+2]]
-            i += 4
+            print("Values: {}".format(intcode[pointer:pointer+4]))
+            intcode[intcode[pointer + 3]] = intcode[intcode[pointer + 1]] +\
+                intcode[intcode[pointer + 2]]
+            pointer += 4
         elif opcode == 2:
-            intcode[intcode[i+3]] = intcode[intcode[i+1]] *\
-                intcode[intcode[i+2]]
-            i += 4
+            print("Values: {}".format(intcode[pointer:pointer+3]))
+            intcode[intcode[pointer + 3]] = intcode[intcode[pointer + 1]] *\
+                intcode[intcode[pointer + 2]]
+            pointer += 4
         elif opcode == 3:
-            pass
+            print("Values: {}".format(intcode[pointer:pointer+1]))
+            intcode[intcode[pointer + 1]] = input
+            pointer += 2
+        elif opcode == 4:
+            output.append(intcode[pointer])
+            pointer += 2
+        else:
+            break
             # def part2():
          #   return 0
 
 
-print("    Part 1 : {}".format(part1()))
-print("    Part 2 : {}".format(part2()))
+print("    Part 1 : {}".format(part1(input=1)))
+#print("    Part 2 : {}".format(part2()))
