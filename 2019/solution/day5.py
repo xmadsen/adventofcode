6dc7f3d1f2f689e7995b044783987413ce8ce8e9
xmadsen <xander.madsen@gmail.com>
2019-12-07 09:32:00 -0500

    Refactor, part 1 is working again

--- 2019/solution/day5.py
+++ 2019/solution/day5.py
@@ -15,117 +15,80 @@ with open(input_file) as file:
 def parse_instruction(instruction):
     instruction = str(instruction).rjust(5, '0')
     opcode = instruction[-2:]
+    # print(instruction)
     param_modes = list(
         map(int, list(''.join(reversed(str(instruction)[:-2])))))
     return int(opcode.strip("0")), param_modes
 
 
+def get_param_values(intcode, pointer, param_modes):
+    values = []
+    for i in range(len(param_modes)):
+        try:
+            values.append(intcode[pointer + i + 1] if param_modes[i]
+                          == 1 else intcode[intcode[pointer + i + 1]])
+        except Exception as e:
+            print(e)
+    return values
+
+
 def part1(input, part2=False):
     output = []
-    # print(input_values)
     intcode = list(map(int, input_values))
-    intcode = [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9]
-    # intcode = [1002, 4, 3, 4, 33]
-    # intcode[0] = 1
     pointer = 0
     while pointer in range(len(intcode)):
         instruction = intcode[pointer]
 
         opcode, param_modes = parse_instruction(instruction)
-        # print("Pointer: {}".format(pointer))
-        # print("Instruction: {}".format(instruction))
-        # print("Opcode: {}".format(opcode))
-        # print("Param_modes: {}".format(param_modes))
-       # print(opcode)
+        print(param_modes)
+        values = get_param_values(intcode, pointer, param_modes)
         if opcode == 99:
             return output
         elif opcode == 1:
-           # print("Values: {}".format(intcode[pointer:pointer+4]))
-            parameter1 = intcode[pointer +
-                                 1] if param_modes[0] == 1 else intcode[intcode[pointer + 1]]
-            parameter2 = intcode[pointer +
-                                 2] if param_modes[1] == 1 else intcode[intcode[pointer + 2]]
-
-            intcode[intcode[pointer + 3]] = parameter1 + parameter2
+            intcode[intcode[pointer + 3]] = values[0] + values[1]
             pointer += 4
-
         elif opcode == 2:
-            #print("Values: {}".format(intcode[pointer:pointer+3]))
-            parameter1 = intcode[pointer +
-                                 1] if param_modes[0] == 1 else intcode[intcode[pointer + 1]]
-            parameter2 = intcode[pointer +
-                                 2] if param_modes[1] == 1 else intcode[intcode[pointer + 2]]
-
-            intcode[intcode[pointer + 3]] = parameter1 * parameter2
+            intcode[intcode[pointer + 3]] = values[0] * values[1]
             pointer += 4
-
         elif opcode == 3:
-            #print("Values: {}".format(intcode[pointer:pointer+1]))
-
-            parameter1 = intcode[pointer +
-                                 1] if param_modes[0] == 1 else intcode[intcode[pointer + 1]]
-
             intcode[intcode[pointer + 1]] = input
             pointer += 2
         elif opcode == 4:
-
-            parameter1 = intcode[pointer +
-                                 1] if param_modes[0] == 1 else intcode[intcode[pointer + 1]]
-            output = parameter1
-            # print("===OUTPUT===")
-            # print(parameter1)
-            # print("===OUTPUT===")
+            output = values[0]
             pointer += 2
-        if part2:
-            #print("Got to Part 2")
+        elif part2:
+            print("Got to Part 2")
+            if opcode == '00225':
+                print(intcode[:pointer])
             if opcode == 5:
                 print(intcode[pointer:pointer+3])
-                parameter1 = intcode[pointer +
-                                     1] if param_modes[0] == 1 else intcode[intcode[pointer + 1]]
-                parameter2 = intcode[pointer +
-                                     1] if param_modes[1] == 1 else intcode[intcode:[pointer + 1]]
-                if parameter1 != 0:
-                    pointer = parameter2
+                if values[0] != 0:
+                    pointer = values[1]
+                else:
+                    pointer += 3
                 print(pointer)
-                print(intcode[pointer])
+                print(intcode[:pointer])
 
             elif opcode == 6:
-
-                parameter1 = intcode[pointer +
-                                     1] if param_modes[0] == 1 else intcode[intcode[pointer + 1]]
-                parameter2 = intcode[pointer +
-                                     1] if param_modes[1] == 1 else intcode[intcode[pointer + 1]]
-                if parameter1 == 0:
-                    pointer = parameter2
+                if values[0] == 0:
+                    pointer = values[1]
+                else:
+                    pointer += 3
 
             elif opcode == 7:
-                parameter1 = intcode[pointer +
-                                     1] if param_modes[0] == 1 else intcode[intcode[pointer + 1]]
-                parameter2 = intcode[pointer +
-                                     1] if param_modes[1] == 1 else intcode[intcode[pointer + 1]]
-                parameter3 = intcode[pointer +
-                                     1] if param_modes[2] == 1 else intcode[intcode[pointer + 1]]
-
-                if parameter1 < parameter2:
-                    intcode[parameter3] = 1
+                if values[0] < values[1]:
+                    intcode[values[2]] = 1
                 else:
-                    intcode[parameter3] = 0
+                    intcode[values[2]] = 0
                 pointer += 4
 
             elif opcode == 8:
-                parameter1 = intcode[pointer +
-                                     1] if param_modes[0] == 1 else intcode[intcode[pointer + 1]]
-                parameter2 = intcode[pointer +
-                                     1] if param_modes[1] == 1 else intcode[intcode[pointer + 1]]
-                parameter3 = intcode[pointer +
-                                     1] if param_modes[2] == 1 else intcode[intcode[pointer + 1]]
-
-                if parameter1 == parameter2:
-                    intcode[parameter3] = 1
+                if values[0] == values[1]:
+                    intcode[values[2]] = 1
                 else:
-                    intcode[parameter3] = 0
+                    intcode[values[2]] = 0
                 pointer += 4
 
 
 print("    Part 1 : {}".format(part1(input=1)))
-print("    Part 2 : {}".format(part1(input=5, part2=True)))
+#print("    Part 2 : {}".format(part1(input=5, part2=True)))
