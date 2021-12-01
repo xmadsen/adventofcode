ce22c67de7540da7fec6ea6737de20fdc3bec07a
Xander Madsen <xander.madsen@gmail.com>
2021-12-01 14:32:28 -0500

    add aoc year 2021 day 1

---
+++ AOC_2021.py
@@ -0,0 +1,45 @@
+from solution import Solution
+from functools import reduce
+from itertools import combinations
+import math
+import numpy as np
+import re
+
+
+class Day1(Solution):
+    def part1(self):
+        # gts = 0
+        # for i, val in enumerate(self.data):
+        #     if i == 0:
+        #         continue
+        #     if val > self.data[i - 1]:
+        #        gts += 1
+
+
+        # return gts
+        return reduce(lambda x, y: x + y, self.data)
+
+    def part2(self):
+        gts = 0
+        i = 0
+        sums = [self.get_window_sum(i)]
+        while 1:
+            i += 1
+            if i > len(self.data) - 3:
+                break
+            sums.append(self.get_window_sum(i))
+            if sums[i] > sums[i - 1]:
+                gts += 1
+
+        return gts
+
+    def get_window_sum(self, start, size=3):
+        return sum(self.data[start:start + size])
+
+
+if __name__ == '__main__':
+    days = [
+        Day1(year=2021, day=1, input_as_ints=True)
+    ]
+    for day in days:
+        print(day)
--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@ import timeit
 
 
 class Solution(ABC):
-    def __init__(self, year, day, input_as_ints=False, delimiter='\n'):
+    def __init__(self, year, day, input_as_ints=False, delimiter="\n"):
         self.year = year
         self.day = day
         self.delimiter = delimiter
@@ -22,22 +22,30 @@ class Solution(ABC):
         def time_function(func, number_of_times, *args, **kwargs):
             r = timeit.timeit(func, number=number_of_times)
             return r / number_of_times
+
         part1_time = time_function(self.part1, 3)
         part2_time = time_function(self.part2, 3)
-        return '''{} - Day {}
+        print(part1_time)
+        print(part2_time)
+        print(self.year)
+        print(self.day)
+        return """
+        {} - Day {}
 ---------------------------------
 Part 1: {:16} | {:7.1n} s
 Part 2: {:16} | {:7.1n} s
-================================= '''.format(self.year, self.day, self.part1(),
-                                             part1_time, self.part2(),
-                                             part2_time)
+================================= 
+""".format(
+            self.year, self.day, self.part1(), part1_time, self.part2(), part2_time
+        )
 
     def get_input_data(self, as_ints=False):
-        url = 'https://adventofcode.com/{}/day/{}/input'.format(
-            self.year, self.day)
+        url = "https://adventofcode.com/{}/day/{}/input".format(self.year, self.day)
 
-        cookies = {'session': '53616c7465645f5fe0e6579e60ac5302025d11b7b8b6f0feb9f59cd525fa36874fe546dc5a0a3e9ad1abd500ab67bb41'}  # noqa
+        cookies = {
+            "session": "53616c7465645f5fe04698120125fb6aa59697fede8522a5b8dcb117b7bd5571db965e03dfc1b4fe04d2621b026e3105"
+        }  # noqa
         text = requests.get(url, cookies=cookies).text
         if as_ints:
-            return list(map(int, text.rstrip('\n').split(self.delimiter)))
-        return text.rstrip('\n').split(self.delimiter)
+            return list(map(int, text.rstrip("\n").split(self.delimiter)))
+        return text.rstrip("\n").split(self.delimiter)
